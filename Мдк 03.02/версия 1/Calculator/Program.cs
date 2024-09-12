f3ce4ad64d7132c27707649235e3020f6b3d7197
Зайцев Эдуард <edikftvu@gmail.com>
2024-09-12 11:21:09 +0400

    Add files via upload

---
+++ Мдк 03.02/версия 1/Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a, b;
+        char oper;
+
+        Console.WriteLine("Добро пожаловать в калькулятор!");
+
+        try
+        {
+            Console.Write("Введите первое число: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите оператор (+ или -): ");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            switch (oper)
+            {
+                case '+':
+                    Console.WriteLine($"{a} + {b} = {a + b}");
+                    break;
+                case '-':
+                    Console.WriteLine($"{a} - {b} = {a - b}");
+                    break;
+                default:
+                    Console.WriteLine("Ошибка: Неправильный оператор. Используйте + или -.");
+                    break;
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка: Вводите числа в правильном формате.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
